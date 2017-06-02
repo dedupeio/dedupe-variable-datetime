3d3dbe143bf724b6fd046b7fed2dd5e900500aa3
Jean Cochrane <jean@jeancochrane.com>
2017-06-02 17:06:53 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 setup(
     name='dedupe-variable-datetime',
     url='https://github.com/datamade/dedupe-variable-datetime',
-    version='0.1.1',
+    version='0.1.2',
     description='DateTime variable type for dedupe',
     packages=['dedupe.variables'],
     install_requires=['dedupe',
