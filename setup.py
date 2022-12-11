39dc1adbfc3c9f545a62c3b420bc41eb2b2ab4c6
Forest Gregg <fgregg@datamade.us>
2022-12-10 19:13:01 -0500

    pyproject.toml

--- setup.py
+++ setup.py
@@ -6,13 +6,4 @@ except ImportError:
     )
 
 
-setup(
-    name="dedupe-variable-datetime",
-    url="https://github.com/datamade/dedupe-variable-datetime",
-    version="1.0.0",
-    description="DateTime variable type for dedupe",
-    packages=["datetimetype"],
-    install_requires=["dedupe", "datetime-distance"],
-    entry_points={"dedupe": ["datetimetype = datetimetype"]},
-    license="The MIT License: http://www.opensource.org/licenses/mit-license.php",
-)
+setup()
